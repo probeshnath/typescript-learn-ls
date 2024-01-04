0b9b153e750c12d9eddd368ecd4c34a5d492f618
probeshnath <probeshn@gmail.com>
2024-01-04 19:45:05 +0600

    ts intro

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
 var myCountry = "Bangladesh";
 console.log(myCountry);
+var myname = function (name) {
+    console.log(name);
+};
+myname("probesh");
--- script.ts
+++ script.ts
@@ -1,2 +1,13 @@
 const myCountry ="Bangladesh"
 console.log(myCountry)
+
+
+// const myname = (name) =>{
+//     console.log(name)
+// }
+
+// myname("probesh")
+
+// What is TypeScript?
+// - Superset of JavaScript created by Microsoft.
+// - Allows static typing
